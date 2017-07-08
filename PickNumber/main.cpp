a878a1c44769c23f1eaa531af8706ff4e03ca57b
Alex Nguyen <alexnguyen@macbook-pro-cua-alex.local>
2017-07-08 14:52:48 -0700

    Initial Commit

---
+++ PickNumber/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PickNumber
+//
+//  Created by Alex Nguyen on 7/8/17.
+//  Copyright © 2017 Tran Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
