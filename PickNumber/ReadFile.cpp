c38eb2fb181b6a1fa0104fcd1e02cf747d2bfaf3
Alex Nguyen <alexnguyen@mbp-cua-alex.lan>
2017-07-16 19:45:25 -0700

    make functions

--- PickNumber/ReadFile.cpp
+++ PickNumber/ReadFile.cpp
@@ -3,9 +3,10 @@
 #include<fstream>
 
 using namespace std;
-int main(){
-    int winNum, winAmount = 0, lostAmount = 0, result = 0, rowCount = 0;
-    ifstream inFile("Ticket.txt");
+
+vector<vector<int>> readNumberFromFile(const string& filename)
+{
+    ifstream inFile(filename);
     vector<int> temprow(2); // input two pairs of columns into vector
     vector<vector<int>> myArray{}; // input 2d vector
     
@@ -13,12 +14,40 @@ int main(){
     while (inFile >> temprow[0] >> temprow[1])
     {
         myArray.push_back(temprow);
-        rowCount++;
     }
+    
+    return myArray;
+}
+
+int computeWinLoss(vector<vector<int>> number,int tossUpNum, int win, int lost)
+{
+    int result;
+    for(int i = 0; i< number.size(); i++)
+    {
+        if (number[i][0] == tossUpNum)
+        {
+            win = number[i][1] * 70;
+        }
+        else
+        {
+            lost = lost + number[i][1];
+        }
+    }
+    result = win - lost;
+    cout << "Your result: " << result;
+    
+    return tossUpNum;
+}
+
+
+int main(){
+    int winNum, winAmount = 0, lostAmount = 0;
+    
+    vector<vector<int>> myArray = readNumberFromFile("Ticket.txt");
     
     // display the numbers from file on screen
     cout << "Numbers from file: " << endl;
-    for (int i = 0; i < rowCount; i++){
+    for (int i = 0; i < myArray.size(); i++){
         for (int j = 0; j < 2; j++){
             std::cout << myArray[i][j] << ' ';
         }
@@ -27,21 +56,7 @@ int main(){
     cout << endl;
     cout << "Enter the toss-up number: "; // enter the win number
     cin >> winNum;
-    
-    for(int i = 0; i< rowCount; i++)
-    {
-        if (myArray[i][0] == winNum)
-        {
-            winAmount = myArray[i][1] * 70; //  number user choose = win number, winAmount = winAmount * 70 - lostAmount
-        }
-        else
-        {
-            lostAmount = lostAmount + myArray[i][1]; //number user choose != win number, the amount will be -lost amounts
-        }
-    }
-    
-    result = winAmount - lostAmount;
-    cout << "Your result: " << result;
+    computeWinLoss(myArray, winNum, winAmount, lostAmount);
     
     cout << endl << endl;
     system("pause");
